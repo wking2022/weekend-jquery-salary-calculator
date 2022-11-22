ffd4ac3ac025dab70dbee9bede0dfe04a2a98d8f
Liam King <techy.willyk3@gmail.com>
2022-11-21 19:38:04 -0600

    Submitting what i have, long weekend. In case i don get to complete more, here's where you can help me debug and figur out where I'm confusin myself.

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 $(document).ready(onReady);
 
 function onReady(){
-    $('#').on('click');
+    $('#butt').on('click', eInfoCollect);
 }
+
+let employeeArr = [];
+
+function eInfoCollect(){
+    let employee = {
+        firstName: $('#fName').val(),
+        lastName: $('#lName').val(),
+        employeeId: $('#ID').val(),
+        jobTitle: $('#jTitle').val(),
+        annualSal: $('#anSal').val(),
+    }
+    (employeeArr).push(employee);
+
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#employeeId').val('');
+    $('#jobTitle').val('');
+    $('#annualSal').val('');
+
+    $('#tBody').append(`
+        <tbody id="tBody">
+            <tr>
+                <td id="fName">${employeeArr.firstName}</td>
+                <td id="lName"><${employeeArr.lastName}/td>
+                <td id="ID">>${employeeArr.employeeId}</td>
+                <td id="jTitle">${employeeArr.jobTitle}</td>
+                <td id="anSal">${employeeArr.annualSal}</td>
+                <td><button class="deleteBut" >Delete</button></td>
+            </tr>`)
+
+}
+
+
+
